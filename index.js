f6e1ca6f6d6c251064a181cb951b00e9c12f353c
Essi Prykäri <essi.prykari@helsinki.fi>
2024-01-18 20:03:43 +0200

    adds deleting and adding a person from the phonebook backend. Exercises 3.4 & 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -44,6 +46,41 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
     }
 
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    console.log(request)
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post(`/api/persons`, (request, response) => {
+    const body = request.body
+    console.log(body)
+    if (!body.name){
+        return response.status(400).json({
+            error:'name missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
