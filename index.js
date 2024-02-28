f34004a44378277adcdda42a04c1b67a9d59b588
Essi Prykäri <essi.prykari@helsinki.fi>
2024-02-28 21:41:29 +0200

    adds MongoDB to the phonebook. Exercise 3.13

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require(`./models/person`)
 
 const morgan = require(`morgan`)
 
 const cors = require(`cors`)
+
+const mongoose = require(`mongoose`)
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -11,31 +15,10 @@ morgan.token('msg', function(request){return JSON.stringify(request.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :msg'))
 app.use(cors())
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number:"39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abromov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-]
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -90,7 +73,7 @@ app.post(`/api/persons`, (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
