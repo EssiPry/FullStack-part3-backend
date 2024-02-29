629703eafb7109c4b7e04e694f7645f3c9b93484
Essi Prykäri <essi.prykari@helsinki.fi>
2024-02-29 20:38:25 +0200

    adds error handling middleware. Exercise 3.16

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const Person = require('./models/person')
 
 const morgan = require(`morgan`)
 
+//error handling middleware
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error:'malformatted id'})
+    }
+
+    next(error)
+}
+
 const cors = require(`cors`)
 
+app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+    }
+
+
 morgan.token('msg', function(request){return JSON.stringify(request.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :msg'))
-app.use(cors())
 
 
 app.get('/api/persons', (request, response) => {
@@ -22,23 +39,24 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
+/*
 app.get('/info', (request, response) => {
     const no_persons = persons.length
     const d = new Date()
     response.send(`<p>Phonebook has info for ${no_persons} people</p> <p>${d}</p>`)
 })
+*/
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+      .then(person => {
+       if (person) {
         response.json(person)
+       } else {
+        response.status(404).end()
+       }
     })
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
+    .catch(error => next(error))
 })
 
 
@@ -61,6 +79,19 @@ app.post(`/api/persons`, (request, response) => {
         response.json(savedPerson)
     })
 })
+
+
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, ()=> {
